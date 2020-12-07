33eb50c36aea1fd02931108ac5a7c1a8853ffb8f
SkiiBoo <45176793+SkiiBoo@users.noreply.github.com>
2020-12-07 17:13:59 -0600

    Add files via upload

---
+++ py_interpreter/py.py
@@ -0,0 +1,25 @@
+import sys
+from py_parser import *
+from py_lexer import *
+
+def usage():
+    sys.stderr.write('Usage: py filename\n')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    filename = sys.argv[1]
+    text = open(filename).read()
+    tokens = py_lex(text)
+    parse_result = py_parse(tokens)
+    if not parse_result:
+        sys.stderr.write('Parse error!\n')
+        sys.exit(1)
+    ast = parse_result.value
+    env = {}
+    ast.eval(env)
+
+    sys.stdout.write('Final variable values:\n')
+    for name in env:
+        sys.stdout.write('%s: %s\n' % (name, env[name]))
---
+++ py_interpreter/py_lexer.py
@@ -0,0 +1,40 @@
+import lexer
+
+RESERVED = 'RESERVED'
+INT      = 'INT'
+ID       = 'ID'
+
+token_exprs = [
+    (r'[ \n\t]+',              None),
+    (r'#[^\n]*',               None),
+    (r'=',                     RESERVED),
+    (r'\(',                    RESERVED),
+    (r'\)',                    RESERVED),
+    (r':',                     RESERVED),
+    #(r';',                     RESERVED),
+    (r'\+',                    RESERVED),
+    (r'-',                     RESERVED),
+    (r'\*',                    RESERVED),
+    (r'/',                     RESERVED),
+    (r'<=',                    RESERVED),
+    (r'<',                     RESERVED),
+    (r'>=',                    RESERVED),
+    (r'>',                     RESERVED),
+    (r'!=',                    RESERVED),
+    (r'==',                    RESERVED),
+    (r'and',                   RESERVED),
+    (r'or',                    RESERVED),
+    (r'not',                   RESERVED),
+    (r'if',                    RESERVED),
+    (r'then',                  RESERVED),
+    (r'else',                  RESERVED),
+    (r'while',                 RESERVED),
+    (r'for',                   RESERVED),
+    #(r'do',                    RESERVED),
+    #(r'end',                   RESERVED),
+    (r'[0-9]+',                INT),
+    (r'[A-Za-z][A-Za-z0-9_]*', ID),
+]
+
+def py_lex(characters):
+    return lexer.lex(characters, token_exprs)
