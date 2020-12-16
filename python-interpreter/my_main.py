eb34f4854f2f399b944bb6c4e29ecd638802f38a
Claire Hough <claire.n.hough@gmail.com>
2020-12-15 22:31:46 -0600

    added descriptive print messages

--- python-interpreter/my_main.py
+++ python-interpreter/my_main.py
@@ -2,17 +2,29 @@ from my_lexer import *
 from my_parser import *
 
 # Arithmetic operators
+print("Addition: 1 + 3 =")
 print(parser.parse(lexer.lex('1 + 3')).eval())
+print("Subtraction: 1 - 3 =")
 print(parser.parse(lexer.lex('1 - 3')).eval())
+print("Multiplication: 1 * 3 =")
 print(parser.parse(lexer.lex('1 * 3')).eval())
+print("Division: 1 / 3 =")
 print(parser.parse(lexer.lex('1 / 3')).eval())
+print("Modulo: 1 % 3 =")
 print(parser.parse(lexer.lex('1 % 3')).eval())
+print("Power: 2 ** 3 =")
 print(parser.parse(lexer.lex('2 ** 3')).eval())
 
 # Conditional Statements
+print("Less Than: 1 < 3 =")
 print(parser.parse(lexer.lex('1 < 3')).eval())
+#print("Less Than or Equal To: 1 <= 3 =")
 #print(parser.parse(lexer.lex('1 <= 3')).eval())
+print("Greater Than: 1 > 3 =")
 print(parser.parse(lexer.lex('1 > 3')).eval())
+#print("Greater Than or Equal To: 1 >= 3 =")
 #print(parser.parse(lexer.lex('1 >= 3')).eval())
+#print("Equal To: 1 == 3 =")
 #print(parser.parse(lexer.lex('1 == 3')).eval())
+#print("Not Equal To: 1 != 3 =")
 #print(parser.parse(lexer.lex('1 != 3')).eval())
